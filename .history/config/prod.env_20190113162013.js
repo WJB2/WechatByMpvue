93913cf8b5faa28d0762404b576fceffc7905fe3
WJB6666 <wujiabao3675@dingtalk.com>
2019-01-13 18:38:45 +0800

    mapvue小程序

---
+++ .history/config/prod.env_20190113162013.js
@@ -0,0 +1,4 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  API_BASE_API:'"https://127.0.0.1:8080/api"'
+}
---
+++ .history/src/pages/index/index_20190113182628.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container" @click="clickHandle">
+    <div class="message">{{msg}}</div>
+    <!-- 使用 click-counter 组件 -->
+    <click-counter :init-num="10" @clicknum="handleClickNum" />
+  </div>
+</template>
+
+<script>
+// 导入 click-counter 组件
+import ClickCounter from "@/components/counter";
+
+export default {
+  // 声明在当前组件下使用 counter-click 组件
+  components: { ClickCounter },
+
+  data() {
+    return {
+      msg: "Hello"
+    };
+  },
+
+  methods: {
+    clickHandle() {
+      this.msg = "Clicked!!!!!!";
+    },
+    handleClickNum(data) {
+      console.log(">>>>>>", data.num);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.message {
+  color: red;
+  padding: 10px;
+  text-align: center;
+}
+</style>
+ 
